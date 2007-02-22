ed0f7722cd31e3c12aa76f7b0250cf419e7cf3b0
Jeff Jacobson <JacobsJ@wsdot.wa.gov>
2007-02-22 19:20:00 +0000

    - Added controls to ArmColumnAdder.FieldSelectionForm that will allow the user to specify a data column from which to get reference dates in addition to specifying a single date for all records.

--- SpellChecker/SpellChecker.cs
+++ SpellChecker/SpellChecker.cs
@@ -38,16 +38,9 @@ namespace ArcMapSpellCheck {
 		/// <summary>Creates a new instance of <see cref="Spellchecker"/>.</summary>
 		private Spellchecker() { }
 
-		///// <summary>
-		///// Checks the spelling of the document currently open in ArcMap.
-		///// </summary>
-		//public static void CheckDocument() {
-		//	Wsdot.ArcGis.ArcMapApplication app = new Wsdot.ArcGis.ArcMapApplication();
-		//	IMxDocument doc = app.Document as IMxDocument;
-		//	CheckDocument(doc);
-		//}
-		
-		/// <summary>
+        // The following directive disables the compiler's warning: "Ambiguity between method 'method' and non-method 'non-method'. Using method group." 
+#pragma warning disable 0467
+        /// <summary>
 		/// Checks the spelling of the given <see cref="IMxDocument"/>.
 		/// </summary>
 		/// <param name="document">An ArcMap document.</param>
@@ -158,109 +151,110 @@ namespace ArcMapSpellCheck {
 			return spellcheckedItems;
 		}
 
-		/// <summary>
-		/// Checks the spelling of the text elements in an ArcMap document.
-		/// </summary>
-		/// <param name="mxDoc">An <see cref="IMxDocument">ArcMap docmuent</see>.</param>
-		/// <param name="wordApp">A <see cref="Word.Application"/> object.</param>
-		/// <param name="wordHasBeenClosed">A <see cref="bool"/> indicating if <paramref name="wordApp"/> has been closed.</param>
-		/// <returns>
-		///		The number of text elements that were spellchecked.  Zero will be returned under the following contiditions:
-		///		<list type="bullet">
-		///			<item><description>No mispelled items were detected.</description></item>
-		///			<item><description>Either <paramref name="maps"/> or <paramref name="wordApp"/> were <see langword="null"/>.</description></item>
-		///			<item><description><paramref name="wordHasBeenClosed"/> is <see langword="true"/>.</description></item>
-		///		</list>
-		///	</returns>
-		private static int CheckSpellingOfTextElements(IMxDocument mxDoc, Word.Application wordApp, ref bool wordHasBeenClosed) {
-			int checkedElementCount = 0;
-			if (wordApp == null || wordHasBeenClosed) return checkedElementCount;
+        /// <summary>
+        /// Checks the spelling of the text elements in an ArcMap document.
+        /// </summary>
+        /// <param name="mxDoc">An <see cref="IMxDocument">ArcMap docmuent</see>.</param>
+        /// <param name="wordApp">A <see cref="Word.Application"/> object.</param>
+        /// <param name="wordHasBeenClosed">A <see cref="bool"/> indicating if <paramref name="wordApp"/> has been closed.</param>
+        /// <returns>
+        ///		The number of text elements that were spellchecked.  Zero will be returned under the following contiditions:
+        ///		<list type="bullet">
+        ///			<item><description>No mispelled items were detected.</description></item>
+        ///			<item><description>Either <paramref name="maps"/> or <paramref name="wordApp"/> were <see langword="null"/>.</description></item>
+        ///			<item><description><paramref name="wordHasBeenClosed"/> is <see langword="true"/>.</description></item>
+        ///		</list>
+        ///	</returns>
+        private static int CheckSpellingOfTextElements(IMxDocument mxDoc, Word.Application wordApp, ref bool wordHasBeenClosed) {
+            int checkedElementCount = 0;
+            if (wordApp == null || wordHasBeenClosed) return checkedElementCount;
 
-			// These values indicate what button was pressed on Word's spell check dialog.
-			const int /*close = -2, ok = -1,*/ cancel = 0;
+            // These values indicate what button was pressed on Word's spell check dialog.
+            const int /*close = -2, ok = -1,*/ cancel = 0;
 
-			// Cast both the PageLayout and FocusMap (Layout view and Data view) to IGraphicsContainers 
-			// and put them into an array.
-			IGraphicsContainer[] gContainers = { 
+            // Cast both the PageLayout and FocusMap (Layout view and Data view) to IGraphicsContainers 
+            // and put them into an array.
+            IGraphicsContainer[] gContainers = { 
 												   mxDoc.PageLayout as IGraphicsContainer, 
 												   mxDoc.FocusMap as IGraphicsContainer 
 											   };
 
-			IElement element;
-			ITextElement textElement;
-			//Word.Application wordApp = new Word.ApplicationClass();
-			object doNotSaveChanges = WdSaveOptions.wdDoNotSaveChanges;
-			// Since C# does not permit optional parameters an instance of Missing must be used in instances where these 
-			// optional parameters were skipped in the VB version of the code.
-			object skipped = Missing.Value;
+            IElement element;
+            ITextElement textElement;
+            //Word.Application wordApp = new Word.ApplicationClass();
+            object doNotSaveChanges = WdSaveOptions.wdDoNotSaveChanges;
+            // Since C# does not permit optional parameters an instance of Missing must be used in instances where these 
+            // optional parameters were skipped in the VB version of the code.
+            object skipped = Missing.Value;
 
-			string currentText;
+            string currentText;
 
-			// Run the MS Word spell check on the text elements in both the Layout and Data views.
-			foreach (IGraphicsContainer gContainer in gContainers) {
-				// If the current IGraphicsContainer is null, skip to the next.
-				if (gContainer == null) {
-					continue;
-				}
+            // Run the MS Word spell check on the text elements in both the Layout and Data views.
+            foreach (IGraphicsContainer gContainer in gContainers) {
+                // If the current IGraphicsContainer is null, skip to the next.
+                if (gContainer == null) {
+                    continue;
+                }
 
-				gContainer.Reset();
-				element = gContainer.Next();
-			
-				Document wdDoc;
+                gContainer.Reset();
+                element = gContainer.Next();
 
-				// Loop through ALL elements and (if they are text elements) check their spelling.
-				while (element != null) {
-					// If the application (Word) has been closed, break out of the loop.
-					if (wordApp == null) {
-						break;
-					}
-					// Cast the current element as an ITextElement.
-					textElement = element as ITextElement;
-					// If the current element is not a text element, go to the next element.
-					if (textElement == null) {
-						element = gContainer.Next();
-						continue;
-					}
-					checkedElementCount++;
-			
-					currentText = textElement.Text;
+                Document wdDoc;
 
-					// Add a new document and add the currently selected text from ArcMap into the Word document.
-					wdDoc = wordApp.Documents.Add(ref skipped, ref skipped, ref skipped, ref skipped);
-					wordApp.Selection.Text = currentText;
+                // Loop through ALL elements and (if they are text elements) check their spelling.
+                while (element != null) {
+                    // If the application (Word) has been closed, break out of the loop.
+                    if (wordApp == null) {
+                        break;
+                    }
+                    // Cast the current element as an ITextElement.
+                    textElement = element as ITextElement;
+                    // If the current element is not a text element, go to the next element.
+                    if (textElement == null) {
+                        element = gContainer.Next();
+                        continue;
+                    }
+                    checkedElementCount++;
 
-					// Create and show the spelling and grammar check dialog.
-					Dialog spellDialog = wordApp.Dialogs.Item(WdWordDialog.wdDialogToolsSpellingAndGrammar);
-					int dialogReturn = spellDialog.Show(ref skipped);
+                    currentText = textElement.Text;
+
+                    // Add a new document and add the currently selected text from ArcMap into the Word document.
+                    wdDoc = wordApp.Documents.Add(ref skipped, ref skipped, ref skipped, ref skipped);
+                    wordApp.Selection.Text = currentText;
+
+                    // Create and show the spelling and grammar check dialog.
+                    Dialog spellDialog = wordApp.Dialogs.Item(WdWordDialog.wdDialogToolsSpellingAndGrammar);
+                    int dialogReturn = spellDialog.Show(ref skipped);
 
 
-					// If the cancel button was clicked on the spell check dialog...
-					if (dialogReturn == cancel) {
-						// Quit word if the spell check was canceled.
-						wordApp.Quit(ref doNotSaveChanges, ref skipped, ref skipped);
-						wordApp = null;
-						wordHasBeenClosed = true;
-						break;
-					}
-					else {
-						// Replace carriage return not followed by a new line with Environment.NewLine.
-						textElement.Text = Regex.Replace(wordApp.Selection.Text, @"\r(?<!\n)", Environment.NewLine);
-					}
+                    // If the cancel button was clicked on the spell check dialog...
+                    if (dialogReturn == cancel) {
+                        // Quit word if the spell check was canceled.
+                        wordApp.Quit(ref doNotSaveChanges, ref skipped, ref skipped);
+                        wordApp = null;
+                        wordHasBeenClosed = true;
+                        break;
+                    } else {
+                        // Replace carriage return not followed by a new line with Environment.NewLine.
+                        textElement.Text = Regex.Replace(wordApp.Selection.Text, @"\r(?<!\n)", Environment.NewLine);
+                    }
 
-					// Close the current document
-					wdDoc.Close(ref doNotSaveChanges, ref skipped, ref skipped);
-					element = gContainer.Next();
-				}
-			}
-			//// Quit MS Word.
-			//if (wordApp != null) {
-			//	wordApp.Quit(ref doNotSaveChanges, ref skipped, ref skipped);
-			//	wordApp = null;
-			//}
-			//mxDoc.ActiveView.Refresh();
-			
-			return checkedElementCount;
-		}
+                    // Close the current document
+                    wdDoc.Close(ref doNotSaveChanges, ref skipped, ref skipped);
+                    element = gContainer.Next();
+                }
+            }
+            //// Quit MS Word.
+            //if (wordApp != null) {
+            //	wordApp.Quit(ref doNotSaveChanges, ref skipped, ref skipped);
+            //	wordApp = null;
+            //}
+            //mxDoc.ActiveView.Refresh();
+
+            return checkedElementCount;
+        }
+#pragma warning restore 0467
+        
 
 		/// <summary>
 		/// A method used for simplifying the process of showing a message box that only has an OK button.
