755478979b88d292f2292e2d499f306ccb8d74b5
Jeff Jacobson <JacobsJ@wsdot.wa.gov>
2010-03-03 18:33:53 +0000

    Converted spell checker to ArcGIS 10 add-in.

---
+++ SpellCheckAddIn/SpellCheckButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.ArcMapUI;
+using ArcMapSpellCheck;
+
+namespace SpellCheckAddIn
+{
+    public class SpellCheckButton : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public SpellCheckButton()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            ////ArcMap.Application.CurrentTool = null;
+            IMxDocument doc = ArcMap.Application.Document as IMxDocument;
+            Spellchecker.CheckDocument(doc);
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
