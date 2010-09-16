fd16bd5ce66ce47ebfe5da43a112b7bc8133c589
Rich Harper <RichHarper@users.noreply.github.com>
2010-09-16 18:20:30 +0000

    Made significant framework changes to get the solution to compile with ArcGIS 10 (see http://help.arcgis.com/en/sdk/10.0/arcobjects_net/conceptualhelp/index.html#/Migrating_ArcGIS_9_3_Desktop_and_Engine_custom_components_to_ArcGIS_10/0001000002m2000000/PlatformCPU/) for some of the changes required.
    Set the version of all projects to 10.0.1.0

--- SpellChecker/AssemblyInfo.cs
+++ SpellChecker/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Washington State Department of Transportation")]
 [assembly: AssemblyProduct("ArcMap Spellchecker")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("© WSDOT 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
@@ -28,7 +28,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("10.0.1.0")]
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
+[assembly: AssemblyFileVersionAttribute("10.0.1.0")]
