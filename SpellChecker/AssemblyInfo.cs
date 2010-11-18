76f33a2ee376a15d71ac47302a2ec6c49af7d1b8
Don Kay <KayD@wsdot.wa.gov>
2010-11-18 22:33:46 +0000

    Added GISWorkbenchCommonAssemblyInfo.cs (containing the common attributes) to the GISWorkbench solution and linked it in all projects within the solution. Removed duplicated attributes (those found in linked GISWorkbenchCommonAssemblyInfo.cs) from AssemblyInfo.cs files in all projects within the solution. Moved AssemblyInfo.cs files into the Properties subfolder of the AsBuiltTools, Wsdot.ArcGIS and Wsdot.Data projects. Incremented the common assembly & file version number (in GISWorkbenchCommonAssemblyInfo.cs) to 9.4.6.0. Incremented version number of GISWorkbench10Setup to 9.4.6.

--- SpellChecker/AssemblyInfo.cs
+++ SpellChecker/AssemblyInfo.cs
@@ -10,26 +10,8 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyTitle("ArcMap Spellchecker")]
 [assembly: AssemblyDescription("Allows users to spellcheck text elements in an ArcMap document using Microsoft Word's spellchecker.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Washington State Department of Transportation")]
 [assembly: AssemblyProduct("ArcMap Spellchecker")]
-[assembly: AssemblyCopyright("© WSDOT 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]		
 
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("10.0.1.0")]
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
-[assembly: AssemblyFileVersionAttribute("10.0.1.0")]
